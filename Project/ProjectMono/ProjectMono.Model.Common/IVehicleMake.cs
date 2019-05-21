5f44e970ff4f5c466ae7e484836c0c2be38fac9a
Ging755 <denis.milic2@gmail.com>
2019-05-21 14:19:58 +0200

    V3

---
+++ Project/ProjectMono/ProjectMono.Model.Common/IVehicleMake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMono.Model.Common
+{
+    public interface IVehicleMake
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Abrv { get; set; }
+        string ImagePath { get; set; }
+    }
+}
---
+++ Project/ProjectMono/ProjectMono.Model.Common/IVehicleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMono.Model.Common
+{
+    public interface IVehicleModel
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Abrv { get; set; }
+        string ImagePath { get; set; }
+
+        int VehicleMakeId { get; set; }
+    }
+}
