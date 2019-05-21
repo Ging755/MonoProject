5c4f3c1305255a6decfb40e6ccc7d9c6dcc6d97a
Ging755 <denis.milic2@gmail.com>
2019-05-21 14:15:10 +0200

    V2

---
+++ Project/ProjectMono/ProjectMono.DAL/Entities/VehicleMakeEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMono.DAL.Entities
+{
+    public class VehicleMakeEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abrv { get; set; }
+        public string ImagePath { get; set; }
+
+        public virtual ICollection<VehicleModelEntity> VehicleModels { get; set; }
+
+        public VehicleMakeEntity()
+        {
+            VehicleModels = new HashSet<VehicleModelEntity>();
+        }
+    }
+}
---
+++ Project/ProjectMono/ProjectMono.DAL/Entities/VehicleModelEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMono.DAL.Entities
+{
+    public class VehicleModelEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abrv { get; set; }
+        public string ImagePath { get; set; }
+
+        public int VehicleMakeId { get; set; }
+        public virtual VehicleMakeEntity VehicleMake { get; set; }
+    }
+}
