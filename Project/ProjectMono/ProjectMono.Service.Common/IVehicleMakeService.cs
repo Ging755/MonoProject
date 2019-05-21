2e8aaef33ffa9a6e1bed81e65d0f2efa6cc6079f
Ging755 <denis.milic2@gmail.com>
2019-05-21 14:28:56 +0200

    V8

---
+++ Project/ProjectMono/ProjectMono.Service.Common/IVehicleMakeService.cs
@@ -0,0 +1,23 @@
+﻿using ProjectMono.Common.PagedResult;
+using ProjectMono.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMono.Service.Common
+{
+    public interface IVehicleMakeService
+    {
+        //Create
+        Task AddVehicleMakeAsync(IVehicleMake entity);
+        //Read
+        Task<IVehicleMake> GetVehicleMake(int id);
+        Task<PagedResult<IVehicleMake>> GetVehicleMakes();
+        //Update
+        Task UpdateVehicleMakeAsync(IVehicleMake entity);
+        //Delete
+        Task DeleteVehicleMakeAsync(IVehicleMake entity);
+    }
+}
---
+++ Project/ProjectMono/ProjectMono.Service.Common/IVehicleModelService.cs
@@ -0,0 +1,23 @@
+﻿using ProjectMono.Common.PagedResult;
+using ProjectMono.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMono.Service.Common
+{
+    public interface IVehicleModelService
+    {
+        //Create
+        Task AddVehicleModelAsync(IVehicleModel entity);
+        //Read
+        Task<IVehicleModel> GetVehicleModel(int id);
+        Task<PagedResult<IVehicleModel>> GetVehicleModels();
+        //Update
+        Task UpdateVehicleModelAsync(IVehicleModel entity);
+        //Delete
+        Task DeleteVehicleModelAsync(IVehicleModel entity);
+    }
+}
